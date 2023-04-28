e284cd00812e86f095c93d1201ea31d7c9f0288e
ephieo <60614102+ephieo@users.noreply.github.com>
2023-04-28 18:34:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import { IndexIcon, MainCont } from './../styled-components/reusables';
-import icon from './../public/index-logo-2.svg';
-import Header from './../components/Header.js';
-import Image from 'next/image';
+// import icon from './../public/index-logo-2.svg';
+// import Header from './../components/Header.js';
+// import Image from 'next/image';
 // import { useState } from 'react';
 
 export default function Home() {
   return (
     <MainCont>
-      <Header />
-      <IndexIcon>
-        <Image src={icon} alt="" />
-      </IndexIcon>
+    <h1> Will be back soon ... </h1>
+//       <Header />
+//       <IndexIcon>
+//         <Image src={icon} alt="" />
+//       </IndexIcon>
     </MainCont>
   );
 }
